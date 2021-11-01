da3fbee6774a145117aab2ee3664e5008acc54fe
Noname <catuananh@gmail.com>
2021-11-01 16:56:14 +0100

    native sql

--- src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
+++ src/main/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepository.java
@@ -1,12 +1,43 @@
 package com.appsdeveloperblog.app.ws.io.repositories;
 
 import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import javax.transaction.Transactional;
+import java.util.List;
 
 @Repository
 public interface UserRepository extends PagingAndSortingRepository<UserEntity, Long> {
     UserEntity findByEmail(String email);
     UserEntity findByUserId(String userId);
     UserEntity findUserByEmailVerificationToken(String token);
+
+    @Query(value="select * from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+            countQuery = "select count(*) from Users u where u.EMAIL_VERIFICATION_STATUS = 'true'",
+            nativeQuery = true)
+    Page<UserEntity> findAllUsersWithConfirmedEmailAddress(Pageable pageableRequest);
+
+    @Query(value="select * from Users u where u.first_name = ?1", nativeQuery = true)
+    List<UserEntity> findUserByFirstName(String firstName);
+
+    @Query(value="select * from Users u where u.last_name = :lastName", nativeQuery = true)
+    List<UserEntity> findUserByLastName(@Param("lastName") String lastName);
+
+    @Query(value="select * from Users u where u.first_name like %:keyword% or u.last_name like %:keyword%", nativeQuery = true)
+    List<UserEntity> findUserByKeyword(@Param("keyword") String keyword);
+
+    @Query(value="select u.first_name, u.last_name from Users u where u.first_name like %:keyword% or u.last_name like %:keyword%", nativeQuery = true)
+    List<Object[]> findUserFirstNameAndLastNameByKeyword(@Param("keyword") String keyword);
+
+    @Transactional
+    @Modifying
+    @Query(value="update users u set u.EMAIL_VERIFICATION_STATUS = :emailVerificationStatus where u.user_id = :userId", nativeQuery = true)
+    void updateUserEmailVerificationStatus(@Param("emailVerificationStatus") boolean emailVerificationStatus,
+                                           @Param("userId") String userId);
 }
---
+++ src/test/java/com/appsdeveloperblog/app/ws/io/repositories/UserRepositoryTest.java
@@ -0,0 +1,188 @@
+package com.appsdeveloperblog.app.ws.io.repositories;
+
+import com.appsdeveloperblog.app.ws.io.entity.AddressEntity;
+import com.appsdeveloperblog.app.ws.io.entity.UserEntity;
+import com.appsdeveloperblog.app.ws.shared.dto.AddressDTO;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class UserRepositoryTest extends Object {
+
+    @Autowired
+    UserRepository userRepository;
+
+    static boolean recordsCreated = false;
+
+    @BeforeEach
+    void setUp() throws Exception{
+        if (!recordsCreated) createdRecord();
+    }
+
+    private void createdRecord() {
+        UserEntity userEntity = new UserEntity();
+        userEntity.setId(1L);
+        userEntity.setFirstName("Sergey");
+        userEntity.setLastName("Phan");
+        userEntity.setUserId("1231sada");
+        userEntity.setEncryptedPassword("xxx");
+        userEntity.setEmail("test@test.com");
+        userEntity.setEmailVerificationToken("afjalfjlkafjlfasf");
+        userEntity.setEmailVerificationStatus(true);
+
+        AddressEntity addressEntity = new AddressEntity();
+        addressEntity.setType("shipping");
+        addressEntity.setAddressId("asdafwefw");
+        addressEntity.setCity("Vancover");
+        addressEntity.setCountry("Canada");
+        addressEntity.setPostalCode("ABC123");
+        addressEntity.setStreetName("123 Street name");
+
+        AddressEntity billingAddressEntity = new AddressEntity();
+        billingAddressEntity.setType("billing");
+        billingAddressEntity.setAddressId("sadafqwew");
+        billingAddressEntity.setCity("Vancover");
+        billingAddressEntity.setCountry("Canada");
+        billingAddressEntity.setPostalCode("ABC123");
+        billingAddressEntity.setStreetName("123 Street name");
+
+        List<AddressEntity> addresses = new ArrayList<>();
+        addresses.add(addressEntity);
+        addresses.add(billingAddressEntity);
+
+        userEntity.setAddresses(addresses);
+
+        userRepository.save(userEntity);
+
+        UserEntity userEntity2 = new UserEntity();
+        userEntity2.setId(2L);
+        userEntity2.setFirstName("Sergey");
+        userEntity2.setLastName("Phan");
+        userEntity2.setUserId("1231sada2");
+        userEntity2.setEncryptedPassword("xxx");
+        userEntity2.setEmail("test1@test.com");
+        userEntity2.setEmailVerificationToken("afjalfjlkasdasd");
+        userEntity2.setEmailVerificationStatus(true);
+
+        AddressEntity addressEntity2 = new AddressEntity();
+        addressEntity2.setType("shipping");
+        addressEntity2.setAddressId("asdafwefw2");
+        addressEntity2.setCity("Vancover");
+        addressEntity2.setCountry("Canada");
+        addressEntity2.setPostalCode("ABC123");
+        addressEntity2.setStreetName("123 Street name");
+
+        AddressEntity billingAddressEntity2 = new AddressEntity();
+        billingAddressEntity2.setType("billing");
+        billingAddressEntity2.setAddressId("sadafqwew2");
+        billingAddressEntity2.setCity("Vancover");
+        billingAddressEntity2.setCountry("Canada");
+        billingAddressEntity2.setPostalCode("ABC123");
+        billingAddressEntity2.setStreetName("123 Street name");
+
+        List<AddressEntity> addresses2 = new ArrayList<>();
+        addresses2.add(addressEntity2);
+        addresses2.add(billingAddressEntity2);
+
+        userEntity2.setAddresses(addresses2);
+
+        userRepository.save(userEntity2);
+
+        recordsCreated = true;
+    }
+
+    @Test
+    final void testGetVerifiedUser(){
+
+        Pageable pageableRequest = PageRequest.of(0, 2);
+        Page<UserEntity> pages = userRepository.findAllUsersWithConfirmedEmailAddress(pageableRequest);
+        assertNotNull(pages);
+
+        List<UserEntity> userEntities = pages.getContent();
+        assertNotNull(userEntities);
+        assertTrue(userEntities.size() == 2);
+    }
+
+    @Test
+    final void testFindUserByFirstName(){
+
+        String firstName="Sergey";
+        List<UserEntity> users = userRepository.findUserByFirstName(firstName);
+        assertNotNull(users);
+        assertTrue(users.size() == 2);
+
+        UserEntity user = users.get(0);
+        assertTrue(user.getFirstName().equals(firstName));
+    }
+
+    @Test
+    final void testFindUserByLastName(){
+
+        String lastName="Phan";
+        List<UserEntity> users = userRepository.findUserByLastName(lastName);
+        assertNotNull(users);
+        assertTrue(users.size() == 2);
+
+        UserEntity user = users.get(0);
+        assertTrue(user.getLastName().equals(lastName));
+    }
+
+    @Test
+    final void testFindUserByKeyword(){
+
+        String keyword="ha";
+        List<UserEntity> users = userRepository.findUserByKeyword(keyword);
+        assertNotNull(users);
+        assertTrue(users.size() == 2);
+
+        UserEntity user = users.get(0);
+        assertTrue(user.getLastName().contains(keyword) ||
+                user.getFirstName().contains(keyword));
+    }
+
+    @Test
+    final void testFindUserFirstNameAndLastNameByKeyword(){
+
+        String keyword="ha";
+        List<Object[]> users = userRepository.findUserFirstNameAndLastNameByKeyword(keyword);
+        assertNotNull(users);
+        assertTrue(users.size() == 2);
+
+        Object[] user = users.get(0);
+
+        String userFirstName = String.valueOf(user[0]);
+        String userLastName = String.valueOf(user[1]);
+
+        assertNotNull(userFirstName);
+        assertNotNull(userLastName);
+
+        System.out.println(userFirstName);
+        System.out.println(userLastName);
+    }
+
+    @Test
+    final void testUpdateUserEmailVerification(){
+
+        boolean newEmailVerification = false;
+        userRepository.updateUserEmailVerificationStatus(newEmailVerification, "1231sada");
+
+        UserEntity storedUserDetails = userRepository.findByUserId("1231sada");
+
+        boolean storedEmailVerificationStatus = storedUserDetails.getEmailVerificationStatus();
+
+        assertTrue(storedEmailVerificationStatus == newEmailVerification);
+    }
+}
